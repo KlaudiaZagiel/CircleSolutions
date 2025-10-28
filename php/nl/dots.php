ddc66c6f7d80a1b41ce41c80c6839455b78ce229
LykhotaIvan <ivan.lykhota@student.nhlstenden.com>
2025-10-28 16:12:08 +0100

    made code looks beter and simpler to read

--- php/nl/dots.php
+++ php/nl/dots.php
@@ -21,49 +21,19 @@ if (isset($_POST['toggle'])) {
     <?php
     if (!empty($_SESSION['light'])) {
         echo "<link rel='stylesheet' href='../../css/darkMode/dots.css'>";
+        echo "<link rel='stylesheet' href='../../css/darkMode/mainpageStyles.css'>";
     } else {
         echo "<link rel='stylesheet' href='../../css/lightMode/dots.css'>";
+        echo "<link rel='stylesheet' href='../../css/lightMode/mainPageStyles.css'>";
     }
     ?>
     <title>D.O.T.S</title>
 </head>
 <body>
-    <header>
-        <div class="backroundimagemaindiv">
-            <div class="headerdivmain">
-                <a href="../../php/nl/mainPage.php">
-                    <img class="headerImagedots" src="../../images/header/circleSolutionsLogo.png" alt="logo">
-                </a>
-                <ul class="headerbuttons">
-                    <li><a href="../../php/nl/dots.php">D.O.T.S</a></li>
-                    <li><a href="../../php/nl/contactPage.php">Contact</a></li>
-                    <li><a href="../../php/nl/aboutUs.php">Over ons</a></li>
-                    <li><a href="../../php/nl/whatWeBuild.php">Oplossing</a></li>
-                    <li>
-                        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-                            <button name="toggle" class="buttonflag buttondarLight">
-                                <img src="<?php
-                                    if (!empty($_SESSION['light'])) {
-                                        echo '../../images/header/darkModeToggle.png';
-                                    } else {
-                                        echo '../../images/header/lightModeToggle.png';
-                                    }
-                                ?>" alt="">
-                            </button>
-                        </form>
-                    </li>
-                    <li>
-                        <a class="buttonflag" href="../../php/en/dots.php">
-                            <img src="../../images/flag.png" alt="flag">
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </header>
+    <?php include '../../php/nl/header.php'; ?>
 
     <div class="maindiv">
-        <hr>
+        <hr class="hrTop">
         <div class="dotsmaintext">
             <p class="bigtextcircle">Circle</p>
             <p class="bigtextdots">Dots</p>
